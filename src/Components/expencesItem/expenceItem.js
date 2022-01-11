c2c498912885ae7df1ed32819ec6de16fcf19acd
prabhashrai02 <prabhashsahaj@gmail.com>
2022-01-11 11:22:47 +0530

    separated date component

--- src/Components/expencesItem/expenceItem.js
+++ src/Components/expencesItem/expenceItem.js
@@ -1,16 +1,10 @@
+import ExpenceDate from '../ExpenceDate/ExpenceDate';
 import './ExpenceItem.css';
 
 function ExpenceItem(props) {
-    const day= props.date.toLocaleString('en-US', {day: '2-digit'});
-    const month= props.date.toLocaleString('en-US', {month: 'long'});
-    const year= props.date.getFullYear();
     return (
         <div className="expence_item">
-            <div>
-                {day} <br/>
-                {month} <br/>
-                {year}
-            </div>
+            <ExpenceDate date={props.date} />
             <div>{props.title}</div>
             <div>{props.amount}</div>
         </div>
