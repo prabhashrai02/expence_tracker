1d2c1f0406e4402d61479fc8dd0cdb7e8b609a5f
prabhashrai02 <prabhashsahaj@gmail.com>
2022-01-11 11:26:27 +0530

    added styling to date component

--- src/Components/ExpenceDate/ExpenceDate.js
+++ src/Components/ExpenceDate/ExpenceDate.js
@@ -1,13 +1,15 @@
+import './ExpenceDate.css';
+
 function ExpenceDate(props) {
     const day= props.date.toLocaleString('en-US', {day: '2-digit'});
     const month= props.date.toLocaleString('en-US', {month: 'long'});
     const year= props.date.getFullYear();
 
     return (
-        <div>
-            <div>{day}</div>
-            <div>{month}</div>
-            <div>{year}</div>
+        <div className="expense_date">
+            <div className="expense_date_day">{day}</div>
+            <div className="expense_date_month">{month}</div>
+            <div className="expense_date_year">{year}</div>
         </div>    
     )
 }
