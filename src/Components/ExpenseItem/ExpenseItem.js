e9357a6f0cb95b707cf7630f6d2a0dfc7ea473c6
prabhashrai02 <prabhashsahaj@gmail.com>
2022-01-11 18:19:55 +0530

    added button to change description

--- src/Components/ExpenseItem/ExpenseItem.js
+++ src/Components/ExpenseItem/ExpenseItem.js
@@ -1,14 +1,23 @@
+import React, { useState } from 'react';
 import ExpenseDate from '../ExpenseDate/ExpenseDate';
 import './ExpenseItem.css';
 import Card from '../Card/Card';
 
 function ExpenceItem(props) {
+
+    const [title, setTitle] = useState(props.title);
+
+    const changetitle = () => {
+        setTitle('Prabhash');
+    }
+
     return (
         <Card className="expense_item">
             <ExpenseDate date={props.date} />
             <div className="expense_item_description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense_item_price">{props.amount}</div>
+                <button onClick={changetitle} >Click me</button>
             </div>
         </Card>
     )
