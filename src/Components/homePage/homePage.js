30e3ee913459bd649ae04fc0c4cff2e0fff9e7b2
prabhashrai02 <prabhashsahaj@gmail.com>
2022-01-12 13:40:15 +0530

    added dynamic rendering

--- src/Components/homePage/homePage.js
+++ src/Components/homePage/homePage.js
@@ -1,28 +1,32 @@
+import React, {useState} from 'react';
 import Navbar from "../Navbar/Navbar";
 import AddExpense from "../AddExpense/AddExpense";
 import './HomePage.css';
 import Expenses from "../Expenses/Expenses";
 
+const expense = [
+    {
+        id: `1`,
+        date: new Date(),
+        title: `new`,
+        amount: 200
+    }
+];
+
 function Homepage() {
-    
-    const expense = [
-        {
-            id: `1`,
-            date: new Date(),
-            title: `new`,
-            amount: 200
-        }
-    ];
+    const [addData, setaddData] = useState(expense);
 
     const saveNewData = (enteredData) => {
-        console.log(enteredData);
-    }
+        setaddData((prevExpence) =>{
+            return [enteredData, ...prevExpence];
+        });
+    };
 
     return (
         <div>
             <Navbar />
             <AddExpense onSaveData={saveNewData} />
-            <Expenses items={expense} />
+            <Expenses items={addData} />
         </div>
     )
 }
